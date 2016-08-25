d1291d364aabf6e891fa0fe1b43cfd07741ea8e9
jasmin47 <alxsanborn@gmail.com>
2016-08-25 10:48:27 -0400

    initial save

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,34 @@
 Rails.application.routes.draw do
+root 'users#index'
 
+get 'users/new', to: 'users#new'
+
+post 'users/', to: 'users#create'
+
+  get 'attractions/create'
+
+  get 'attractions/edit'
+
+  get 'attractions/update'
+
+  get 'attractions/destroy'
+
+  get 'attractions/index'
+
+  get 'attractions/show'
+
+
+
+
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+
+
+  get 'users/show'
 
 end
